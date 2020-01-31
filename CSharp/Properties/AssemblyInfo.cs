61048ef1a0e709454ec239cf449b8c73b85364d7
vintasoft <D:\The Bat!>
2020-01-31 11:41:40 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("9.0.0.9")]
-[assembly: AssemblyFileVersion("9.0.0.9")]
+[assembly: AssemblyVersion("9.0.0.11")]
+[assembly: AssemblyFileVersion("9.0.0.11")]
